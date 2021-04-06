34ab884916586f1f84a33cad1dd8d4369afd1cf6
saee0123 <saee0123@knou.ac.kr>
2021-04-06 21:36:50 +0900

    210406 스터디 연습문제 1

---
+++ 210406_1/210406_1/Source.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+void main() {
+	int _inumber10 = 10;
+	int _inumber30 = 30;
+	int _inumber200 = 200;
+	int _inumber500 = 500;
+
+	printf("%d \n", _inumber10);
+	printf("%d \n", _inumber30);
+	printf("%d \n", _inumber200);
+	printf("%d \n", _inumber500);
+
+	fgetc(stdin);
+}
